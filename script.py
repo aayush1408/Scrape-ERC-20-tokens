ac745e92016a00d64553a8ab427ddd128f511df3
aayush1408 <aayushjaiswal0114@gmail.com>
2018-07-21 12:56:22 +0530

    added collected data to dictionary

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
+
 
 # empty lists for storing the scraped data
 token_names = []
@@ -9,7 +9,7 @@ token_images = []
 contract_address = []
 decimal = []
 supply_amount = []
-final_data = []
+complete_data = []
 
 # url to navigate to
 str = 'https://etherscan.io/'
@@ -30,23 +30,22 @@ token_description_nodes = soup.select('small font')
 token_images_nodes  = soup.find_all('img')
 
 # Storing the values in the empty lists
-# list of nodes for token description
 for token in token_name_nodes:
   token_names.append(token.getText())
-print(token_names)
+print(len(token_names))
 
 for token in token_description_nodes:
   token_descriptions.append(token.getText())
-print(token_descriptions)
+print(len(token_descriptions))
 
 for token in token_images_nodes:
-  token_images.append(token.get('src'))
+  token_images.append(str + token.get('src'))
 print(token_images)
 
 # Message for the command line
-print('-------------------------------------------------------------------------------')
+print('---------------------------------------------------------------------------------------------------------------')
 print('Please wait scraping the remaining data')
-print('-------------------------------------------------------------------------------')
+print('---------------------------------------------------------------------------------------------------------------')
 
 # navigation to different page and scraping the data
 # iterating and getting the link for each erc20 token
@@ -59,19 +58,37 @@ for link in token_name_nodes:
     res_of_another_page = requests.get(link_res)
     soupy = BeautifulSoup(res_of_another_page.text,'html.parser')
 
-    # Getting the desired nodes
-    row1 = soupy.find('tr', {'id':'ContentPlaceHolder1_tr_valuepertoken'})
-    row2 = soupy.find('tr', {'id':'ContentPlaceHolder1_trContract'})
-    row3 = soupy.find('tr', {'id':'ContentPlaceHolder1_trDecimals'})
+    try:
+        # Getting the desired nodes
+        row1 = soupy.find('tr', {'id':'ContentPlaceHolder1_tr_valuepertoken'})
+        row2 = soupy.find('tr', {'id':'ContentPlaceHolder1_trContract'})
+        row3 = soupy.find('tr', {'id':'ContentPlaceHolder1_trDecimals'})
 
-    #Appending every value to the list
-    supply_amount.append(row1.find_previous_siblings()[0].findAll('td')[1].getText()[1:-1])
-    contract_address.append(row2.findAll('td')[1].getText())
-    decimal.append(row3.findAll('td')[1].getText()[1:-1])
-    print(row1.find_previous_siblings()[0].findAll('td')[1].getText()[1:-1])    
-    print(row2.findAll('td')[1].getText()[1:-1])
-    print(row3.findAll('td')[1].getText()[1:-1])
+        #Appending every value to the list
+        supply_amount.append(row1.find_previous_siblings()[0].findAll('td')[1].getText()[1:-1])
+        contract_address.append(row2.findAll('td')[1].getText())
+        decimal.append(row3.findAll('td')[1].getText()[1:-1])
+        print(row1.find_previous_siblings()[0].findAll('td')[1].getText()[1:-1])    
+        print(row2.findAll('td')[1].getText()[1:-1])
+        print(row3.findAll('td')[1].getText()[1:-1])
+
+    except AttributeError:
+        print('Something went wrong with the network,please try again!!')
+
 
 print(contract_address)
 print(decimal)
 print(supply_amount)
+
+
+# Message for the command line
+print('-------------------------------------------------------------------------------------------------------------')
+print('Please wait collecting the whole data...')
+print('-------------------------------------------------------------------------------------------------------------')
+
+
+for i in range(49):
+    dict = {'serial_no':i,'name':token_names[i],'description':token_descriptions[i],'contract':contract_address[i],'decimal':decimal[i],'supply':supply_amount[i]}
+    complete_data.append(dict)
+
+print(complete_data)    
